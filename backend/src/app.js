baeeacd951df36d99ba7869150980e2ee594775b
SATYAM  SINGH <149295313+Satyam8085@users.noreply.github.com>
2026-04-25 20:46:25 +0530

    vercel update

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://erp-system-lemon-sigma.vercel.app",
   })
 );
 app.use(express.json());
